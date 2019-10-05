8780446ca0d1e0854a6a31b72e55564a2b66fc2e
hihozhou <hihozhou@gmail.com>
2019-10-05 17:55:16 +0800

    json忽略密码

--- app/models/admin.go
+++ app/models/admin.go
@@ -11,7 +11,7 @@ type Admin struct {
 	ID        int       `gorm:"primary_key;AUTO_INCREMENT" json:"id"`       //主键id
 	Phone     string    `gorm:"type:varchar(15);unique_index" json:"phone"` //手机号码
 	Nickname  string    `gorm:"type:varchar(80)" json:"nickname"`           //昵称
-	Password  string    `gorm:"type:varchar(32)" json:"password"`           //密码，加密后
+	Password  string    `gorm:"type:varchar(32)" json:"-"`                  //密码，加密后，转json后忽略
 	CreatedAt time.Time `json:"created_at"`                                 //创建时间
 	UpdatedAt time.Time `json:"updated_at"`                                 //删除时间
 	datebase.SoftDeletes
